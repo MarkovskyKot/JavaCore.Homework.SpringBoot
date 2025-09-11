1f86c0238ead058c380f6ecd9b213bc965e734cd
MarkovskyKot <0postaldude0@.gmail.com>
2025-09-11 21:48:31 +0700

    Initial commit

---
+++ src/test/java/pro/skypro/skyshop/SkyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skypro.skyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
